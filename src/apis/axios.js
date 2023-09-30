f6bac7151553b696f69499ab585f4ed3c0c81591
six-standard <dbrrl1127@gmail.com>
2023-10-01 02:19:30 +0900

    특정 내용 제거 (baseURL, 토큰)

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "http://3.37.11.107:8080",
+  baseURL: "",
   timeout: "3000",
   headers: {
-    Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJnYW5nIiwidHlwZSI6ImFjY2VzcyIsImlhdCI6MTY5NjA5MDM3NiwiZXhwIjoxNjk2MDkzOTc2fQ.QdqiRm9isMxRE6gDh8YoKY16R_lgxWwj531d-c7gYDs"
+    Authorization: ""
   }
 })

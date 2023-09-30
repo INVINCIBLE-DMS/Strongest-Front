9c59d7df44a39a834a6c005c8d7a9bec0711b6f8
dutexion <dutexion@gmail.com>
2023-10-01 02:27:35 +0900

    인스턴스 baseurl 삽입 및 header 제거

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "",
+  baseURL: "3.37.11.107:8080",
   timeout: "3000",
-  headers: {
-    Authorization: ""
-  }
-})
+});
